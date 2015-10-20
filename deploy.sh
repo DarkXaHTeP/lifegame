814691d067cd26ba44401bce185498d3688729d0
Volodymyr Osmak <darkxahtep@gmail.com>
2015-10-21 00:49:44 +0300

    Added deployment script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -o errexit #abort if any command fails
+
+REPO = $GH_TOKEN@github.com/DarkXaHTeP/lifegame.git
+cd public
+git init
+git config user.name "travis CI deploy"
+git config user.email "travis"
+echo "Configured repo for deploy"
+git add .
+git commit -m "Deployed to github pages"
+git push --force --quiet $REPO master:gh-pages
+echo "Successfully pushed changes to GitHub Pages"
