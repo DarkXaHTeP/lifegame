c4778ee9da633b8de3a4bdf87f478ac98d71960c
Anastasiia Skorobohata <yarostbaklajana@gmail.com>
2017-02-04 18:59:45 +0200

    Added production build mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,71 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
-module.exports = {
-    context: path.join(__dirname, '/src'),
-    entry: './index',
-    output: {
-        path: path.join(__dirname, '/public'),
-        filename: 'bundle.js'
-    },
+module.exports = function (env) {
+    const isProd = env === "prod";
+    if (isProd) {
+        process.env.NODE_ENV = "production";
+    }
 
-    resolve: {
-        extensions: ['.jsx', '.js']
-    },
+    console.log(`This build is in production mode: ${isProd}`);
 
-    module: {
-        rules: [
-            {
-                test: /\.(jsx|js)(\?.*$|$)/,
-                use: [
-                    {
-                        loader: 'babel-loader'
-                    }
-                ],
-                exclude:['node_modules']
-            },
+    const config = {
+        context: path.join(__dirname, '/src'),
+        entry: './index',
+        output: {
+            path: path.join(__dirname, '/public'),
+            filename: 'bundle.js'
+        },
 
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: 'html-loader'
-                    }
-                ]
-            }
+        resolve: {
+            extensions: ['.jsx', '.js']
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.(jsx|js)(\?.*$|$)/,
+                    use: [
+                        {
+                            loader: 'babel-loader'
+                        }
+                    ],
+                    exclude: ['node_modules']
+                },
+
+                {
+                    test: /\.html$/,
+                    use: [
+                        {
+                            loader: 'html-loader'
+                        }
+                    ]
+                }
+            ]
+        },
+
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "index.html",
+                filename: "index.html"
+            })
         ]
-    },
+    }
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "index.html",
-            filename: "index.html"
-        })
-    ]
+    if (isProd) {
+        config.plugins.push(
+            new webpack.DefinePlugin({
+                'process.env': {
+                    'NODE_ENV': JSON.stringify('production')
+                }
+            }),
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false
+                }
+            })
+        )
+    }
+    return config;
 }
