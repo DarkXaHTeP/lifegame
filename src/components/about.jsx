0126fbf301d0d6c44d8cf8ed784d2d1a47f62db6
Anastasiia Skorobohata <yarostbaklajana@gmail.com>
2017-02-07 00:14:30 +0200

    Added routing between about and game pages

---
+++ src/components/about.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>Bla bla</div> 
+    )
+}
+
+export default About;
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,42 +1,18 @@
 import React from 'react';
-import lifeStore from '../stores/lifeStore';
-import Board from './board';
-import ControlCenter from './controlCenter';
+import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, Redirect } from 'react-router';
+import Game from './game'
+import About from './about';
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = this.getState();
-
-        let { width, height } = lifeStore.getBoardSize();
-        this.boardWidth = width;
-        this.boardHeight = height;
-
-        lifeStore.on("change", () => {
-            this.setState(this.getState())
-        });
-    }
-
-    getState() {
-        return {
-            board: lifeStore.getBoard(),
-            isGameRunning: lifeStore.isGameRunning()
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <ControlCenter running={ this.state.isGameRunning }
-                               boardWidth={ this.boardWidth }
-                               boardHeight={ this.boardHeight }/>
-
-                <div className='board-container'>
-                    <Board data={ this.state.board }/>
-                </div>
-            </div>)
-    }
+function App() {
+    return (
+        <Router history={hashHistory}>
+            
+            <Route path='/game' component={Game} />
+            <Route path='/about' component={About} />
+            <Redirect from='*' to='/game' />
+        </Router>
+    )
 }
 
 export default App;
